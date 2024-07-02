d9d7c01810117699fec37e773e5696f2c6e77d88
Anirudh <anirudhvyas26@gmail.com>
2024-07-02 14:52:41 +0530

    midway icon issue api

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -1,10 +1,32 @@
 import { Search } from "@mui/icons-material";
-import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
 import "./weather.css";
-import WaterIcon from '@mui/icons-material/Water';
-import AirIcon from '@mui/icons-material/Air';
+import WaterIcon from "@mui/icons-material/Water";
+import AirIcon from "@mui/icons-material/Air";
+import { useEffect, useState } from "react";
 
 const Weather = () => {
+  const [weatherData, setWeatherData] = useState(false);
+  const search = async (city) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
+
+      const response = await fetch (url);
+      const data = await response.json();
+      console.log(data);
+      setWeatherData({
+        humidity:data.main.humidity,
+        windSpeed:data.wind.speed,
+        temperature:data.main.temp,
+      })
+
+    } catch (error) {
+            console.log(error)
+    }
+  };
+  useEffect(()=>{
+    search("Pune");
+  },[])
   return (
     <div className="weather">
       <div className="search-bar">
@@ -18,16 +40,16 @@ const Weather = () => {
         <p className="temperature">24°C</p>
         <p className="location">Pune</p>
         <div className="weather-data">
-            <div className="col">
-                <WaterIcon className="humid"/>
-                <p className="percentage">91%</p>
-                <span className="feel">Humidity</span>
-            </div>
-             <div className="col">
-                <AirIcon className="airIcon"/>
-                <p className="percentage">3.6Km/hr</p>
-                <span className="feel">Wind Speed</span>
-            </div>
+          <div className="col">
+            <WaterIcon className="humid" />
+            <p className="percentage">91%</p>
+            <span className="feel">Humidity</span>
+          </div>
+          <div className="col">
+            <AirIcon className="airIcon" />
+            <p className="percentage">3.6Km/hr</p>
+            <span className="feel">Wind Speed</span>
+          </div>
         </div>
       </div>
     </div>
