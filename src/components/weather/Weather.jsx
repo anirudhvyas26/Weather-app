d0d6fa0239f53c32550017b622351855a9796745
Anirudh <anirudhvyas26@gmail.com>
2024-07-02 17:38:14 +0530

    completed

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -13,23 +13,31 @@ const Weather = () => {
 "01d":"https://openweathermap.org/img/wn/01d@2x.png",
 "01n":"https://openweathermap.org/img/wn/01n@2x.png",
 "02d":"https://openweathermap.org/img/wn/02d@2x.png",
-"03d":"https://openweathermap.org/img/wn/10d@2x.png",
-"03n":"https://openweathermap.org/img/wn/10d@2x.png",
-"04d":"https://openweathermap.org/img/wn/10d@2x.png",
-"04n":"https://openweathermap.org/img/wn/10d@2x.png",
-"09d":"https://openweathermap.org/img/wn/10d@2x.png",
-"09n":"https://openweathermap.org/img/wn/10d@2x.png",
+"03d":"https://openweathermap.org/img/wn/03d@2x.png",
+"03n":"https://openweathermap.org/img/wn/03n@2x.png",
+"04d":"https://openweathermap.org/img/wn/04d@2x.png",
+"04n":"https://openweathermap.org/img/wn/04n@2x.png",
+"09d":"https://openweathermap.org/img/wn/09d@2x.png",
+"09n":"https://openweathermap.org/img/wn/09n@2x.png",
 "10d":"https://openweathermap.org/img/wn/10d@2x.png",
-"10n":"https://openweathermap.org/img/wn/10d@2x.png",
-"13d":"https://openweathermap.org/img/wn/10d@2x.png",
-"13n":"https://openweathermap.org/img/wn/10d@2x.png",
+"10n":"https://openweathermap.org/img/wn/10n@2x.png",
+"13d":"https://openweathermap.org/img/wn/13d@2x.png",
+"13n":"https://openweathermap.org/img/wn/13n@2x.png",
   }
   const search = async (city) => {
+    if(city===""){
+      alert("Enter the City Name")
+      return;
+    }
     try {
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
 
       const response = await fetch (url);
       const data = await response.json();
+      if(!response.ok){
+        alert(data.message);
+        return;
+      }
       console.log(data);
       const icon = allIcons[data.weather[0].icon] || WbSunnyIcon;
       setWeatherData({
@@ -41,7 +49,8 @@ const Weather = () => {
       })
 
     } catch (error) {
-            console.log(error)
+      setWeatherData(false);
+            console.error("Error in Fetching Data")
     }
   };
   useEffect(()=>{
@@ -54,8 +63,10 @@ const Weather = () => {
         <div className="icon-container">
           <Search className="icon" onClick={()=>search(inputRef.current.value)} />
         </div>
+
       </div>
-      <div className="weather-info">
+      {weatherData?<>
+        <div className="weather-info">
         {/* <WbSunnyIcon className="weather-icon" /> */}
         <img src={weatherData.icon} alt="" className="weather-icon"/>
         <p className="temperature">{weatherData.temperature}°C</p>
@@ -73,6 +84,8 @@ const Weather = () => {
           </div>
         </div>
       </div>
+      </>:<></>}
+     
     </div>
   );
 };
